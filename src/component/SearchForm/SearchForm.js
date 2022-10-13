8de77946a3c29a8f88adf4c44c24b02b56501551
Rifqi Desfriansyah <rifqides21@gmail.com>
2022-10-13 19:08:45 +0700

    adding serach function

--- src/component/SearchForm/SearchForm.js
+++ src/component/SearchForm/SearchForm.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./SearchForm.css";
 
 const SearchForm = () => {
+
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <>
       <div class="container">
         <div class="row height d-flex justify-content-center align-items-center">
           <div class="col-md-8">
             <div class="search">
-              <input type="text" class="form-control"></input>
-              <button class="btn btn-sm btn-primary">Search</button>
+              <input 
+                type="text" 
+                className="form-control" 
+                onChange={(e) => {
+                  setSearchInput(e.target.value);
+                }}
+              />
+              <Link state={{
+                searchKey: searchInput
+              }} to={'/detail/itemsbycategory'} class="btn btn-sm btn-primary">Search</Link>
             </div>
             
           </div>
