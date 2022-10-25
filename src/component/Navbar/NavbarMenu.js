302785e0d70566bc6ad21ba4bae44e5c6cda32f7
Rifqi Desfriansyah <rifqides21@gmail.com>
2022-10-25 21:23:25 +0700

    fix login feature

--- src/component/Navbar/NavbarMenu.js
+++ src/component/Navbar/NavbarMenu.js
@@ -1,32 +1,59 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../../images/Itemui_logo.jpg"
 import "./Navbar.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../UserContext";
 
 function NavbarMenu() {
+  const navigate = useNavigate();
+  const {user, setUser} = useContext(UserContext);
+
+  function logoutHandler(){
+    setUser(null);
+    navigate('/')
+  }
+  
   return (
     <nav className="navbar navbar-expand-lg mb-2 p-2">
     <div className="container">
-    <a className="navbar-brand" href="#">Itemui</a>
+    <a className="navbar-brand">
+      Itemui
+    </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2">
-        <li className="nav-item">
+            <li className="nav-item">
               <Link to='/' className="nav-link text">Home</Link>
             </li>
             <li className="nav-item">
               <Link to='/about' className="nav-link text">About</Link>
             </li>
-            <li className="text nav-item">
-              
-              <Link to='/pages/register' className="nav-link text">Register</Link>
-            </li>
-            <li className="text nav-item">
-              <Link to='/pages/login' className="nav-link text">Login</Link>
-            </li>
+
+            {(user != null) ? (
+              <>
+                <li className="text nav-item">
+                  <Link to='/' className="nav-link text">Upload Barang</Link>
+                </li>
+                <li className="text nav-item">
+                  <Link to='#' onClick={logoutHandler} className="nav-link text">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="text nav-item">
+                  <Link to='/pages/register' className="nav-link text">Register</Link>
+                </li>
+                <li className="text nav-item">
+                  <Link to='/pages/login' className="nav-link text">Login</Link>
+                </li>
+              </> 
+            )}
+
       </ul>
     </div>
   </div>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,50 +1,78 @@
-import React, { Component } from "react";
+import React, { Component, useContext, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import "../LoginRegister.css";
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div className="container_login">
-        <form>
-          <h3>Sign In</h3>
-          <div className="mb-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="mb-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="mb-3">
-            <div className="custom-control custom-checkbox">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
-              />
-              <label className="custom-control-label" htmlFor="customCheck1">
-                Remember me
-              </label>
-            </div>
-          </div>
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right">
-            Forgot <a href="/">password?</a>
-          </p>
-        </form>
-      </div>
-    );
+export default function Login(){
+
+  const {user, setUser} = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const data = [
+    {
+      id: 1,
+      username: "tes",
+      email: "rifqides12@gmail.com",
+      password: "123"
+    }
+  ]
+
+  const [details, setDetails] = useState({
+    email: "",
+    password: ""
+  })
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const checkUser = data.filter((user) => {
+      return user.email === details.email && user.password == details.password;
+    });
+    if (checkUser) {
+      const dataUserLog = checkUser[0];
+      setUser(dataUserLog);
+      navigate('/');
+    }
+    console.log(user);
   }
+
+  return (
+    <div className="container_login container text-start col-md-4 col-9">
+      <form onSubmit={submitHandler}>
+        <h3>Sign In</h3>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="Enter email"
+            onChange={
+              e => setDetails({...details, email: e.target.value})
+            }
+            value={details.email}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Enter password"
+            onChange={
+              e => setDetails({...details, password: e.target.value})
+            }
+            value={details.password}
+            required
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
